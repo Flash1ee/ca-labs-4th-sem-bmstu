3896d6510bfb3e155dd4ca9ea743b48ce74d2760
Dmitry Varin <varindv@gmail.com>
2021-05-05 01:20:41 +0300

    add calculate funcs

---
+++ lab_05/src/integral.py
@@ -0,0 +1,45 @@
+from math import cos, sin, exp, pi
+from scipy.special import roots_legendre
+from typing import Callable as Call
+
+
+class Integral:
+    def __init__(self, lm: list[list[float]], n: list[int], fn: list[int]):
+        self.lm = lm
+        self.n = n
+        self.func_one = Integral.simpson if (fn[0] == 1) else Integral.gauss
+        self.func_two = Integral.simpson if (fn[1] == 1) else Integral.gauss
+
+    def __call__(self, p: float) -> float:
+        f = Integral.__integrated(p)
+
+        inner = lambda x: self.func_two(lambda val_one: f(x, val_one), self.lm[1][0], self.lm[1][1], self.n[1])
+        integ = lambda: self.func_one(inner, self.lm[0][0], self.lm[0][1], self.n[0])
+
+        return integ()
+
+    @staticmethod
+    def simpson(f: Call[[float], float], a: float, b: float, n: int) -> float:
+        if n < 3 or not n % 2:
+            raise Exception("Wrong value n")
+
+        h = (b - a) / (n - 1.0)
+        x = a
+        res = 0.0
+        for i in range((n - 1) // 2):
+            res += f(x) + 4 * f(x + h) + f(x + 2 * h)
+            x += 2 * h
+        return res * h / 3
+
+    @staticmethod
+    def gauss(f: Call[[float], float], a: float, b: float, n: int) -> float:
+        def pToV(p: float, c: float, d: float) -> float:
+            return (d + c) / 2 + (d - c) * p / 2
+
+        x, w = roots_legendre(n)
+        return sum([(b - a) / 2 * w[i] * f(pToV(x[i], a, b)) for i in range(n)])
+
+    @staticmethod
+    def __integrated(p: float) -> Call[[float, float], float]:
+        u = lambda x, y: 2 * cos(x) / (1 - sin(x) ** 2 * cos(y) ** 2)
+        return lambda x, y: 4 / pi * (1 - exp(-p * u(x, y))) * cos(x) * sin(x)
--- lab_05/src/main.py
+++ lab_05/src/main.py
@@ -3,11 +3,35 @@ from math import pi
 import view
 import integral
 
+
 def main() -> None:
+    tetta = [0.05, 0.05, 10.0]
 
+    ns, ms = [], []
+    md1s, md2s = [], []
+    ints = []
 
+    terminate = '0'
+    while terminate == '0':
+        try:
+            ns.append(int(input("Input N: ")))
+            ms.append(int(input("Input M: ")))
+            param = float(input("Enter parameter: "))
+            print("Entry integration mode (0 - Gauss, 1 - Simpson)")
+            md1s.append(int(input("Outer integration mode: ")))
+            md2s.append(int(input("Inner integration mode: ")))
+        except ValueError:
+            print("Invalid input data. Program is terminated.")
+            return
 
+        lm = [[0, pi / 2], [0, pi / 2]]
 
+        ints.append(integral.Integrate(lm, [ns[-1], ms[-1]], [md1s[-1], md2s[-1]]))
+
+        print("Result with {:.2f} as a parameter is {:.5f}".format(tetta, ints[-1](tetta)))
+
+        end = input("If you want stop execution, entry not 0?: ")
+    view.plot(ints, tetta, ns, ms, md1s, md2s)
 
 
 if __name__ == '__main__':
