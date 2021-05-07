a599ae3cf1746b09e88fdf01761a9034519ad110
Dmitry Varin <varindv@gmail.com>
2021-05-07 11:01:41 +0300

    complete lab_06

--- lab_06/src/main.py
+++ lab_06/src/main.py
@@ -1,5 +1,5 @@
 from typing import List, Tuple
-
+from differ import Differ
 
 filename = "../data/data.txt"
 
@@ -17,6 +17,19 @@ def read_data(filename: str) -> Tuple[List[float], List[float]]:
 def main() -> None:
     x, y = read_data(filename)
 
+    h = 1.0
+
+    Differ.print_init("X              :", x)
+    Differ.print_init("Y              :", y)
+    Differ.print_res("Onesided       :", Differ.left(y, h))
+    Differ.print_res("Center         :",
+                             Differ.center(y, h))
+    Differ.print_res("Second Range   :",
+                             Differ.second_runge(y, h, 1))
+    Differ.print_res("Aligned params :",
+                             Differ.aligned_coeffs(x, y))
+    Differ.print_res("Second oneSided:",
+                             Differ.second_left(y, h))
 
 
 if __name__ == '__main__':
